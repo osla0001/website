05506c8ba6125584517e826ea7d430f6accb83f3
Oscar Laursen <osla0001@stud.kea.dk>
2025-01-03 18:09:17 +0100

    smårettelser

---
+++ js/gif.js
@@ -0,0 +1,15 @@
+// Find alle elementer med klassen "gif1"
+const gifs = document.querySelectorAll(".gif1");
+
+// Gå gennem hvert element og tilføj event listeners
+gifs.forEach((gif) => {
+  // Når musen holdes over elementet, skift billedet til GIF
+  gif.addEventListener("mouseenter", function () {
+    gif.src = "../imgs/folder2.gif"; // Skift til GIF
+  });
+
+  // Når musen fjernes fra elementet, skift billedet tilbage til PNG
+  gif.addEventListener("mouseleave", function () {
+    gif.src = "../imgs/folder1.png"; // Skift tilbage til PNG
+  });
+});
